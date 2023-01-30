de6426aeea38c224b626cd2d29df2d762290edab
umutnl <96056187+umutnl@users.noreply.github.com>
2023-01-30 19:23:34 +0100

    Initial commit

---
+++ src/components/HeroImage.js
@@ -0,0 +1,21 @@
+//rce
+// with props you can pas data
+
+
+import React, { Component } from 'react'
+import './HeroImageStyles.css'
+
+class HeroImage extends Component {
+    render() {
+        return (
+            <div className='hero-img'>
+                <div className='heading'>
+                    <h1>{this.props.heading}</h1>
+                    <p>{this.props.text}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HeroImage
